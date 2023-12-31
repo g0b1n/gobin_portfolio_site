07617b0a7c92e06f02080ff8a8037a15a0c4cbbb
Gobin <gobin@gobinmbp.mynetworksettings.com>
2023-12-31 15:09:50 -0500

    my portfolio first push

---
+++ static/app.js
@@ -0,0 +1,43 @@
+function openModal(imgElement) {
+    // Get the modal
+    var modal = document.getElementById("photoModal");
+
+    // Get the image and insert it inside the modal
+    var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = imgElement.src;
+
+    // Prevent event bubbling when clicking on the image
+    modalImg.onclick = function(event) {
+        event.stopPropagation();
+    }
+
+    // Close the modal if the user clicks outside the image
+    modal.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
+// Close modal when the user clicks on the cancel button
+document.getElementById("cancelButton").onclick = function() {
+    document.getElementById("photoModal").style.display = "none";
+}
+
+// Close modal when the user clicks on the close icon
+document.getElementById("closeModal").onclick = function() {
+    document.getElementById("photoModal").style.display = "none";
+}
+
+
+// highlight nav tags to indicate the current page
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navLinks = document.querySelectorAll('.my_nav_link');
+    const currentPath = window.location.pathname;
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+});
